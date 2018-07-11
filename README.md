25070eddd5887ad3e5662be6b3057a9f1b480a6a
level2player <576102544@qq.com>
2018-07-11 16:04:41 +0800

    V2

---
+++ README.md
@@ -0,0 +1,8 @@
+# go-t2sdkEx
+恒生电子周边接口sdk二次封装,go版本
+
+# t2sdkEx动态库源码
+https://github.com/level2player/t2sdkEx
+
+# 恒生t2sdk介绍
+英文全称TCP 2 Software Development Kit。T2SDK是接入金融基础件2.0的客户端开发包，因为该开发包底层采用TCP协议，取名为“T2SDK”。T2SDK以dll（Windows下）或so（Linux下）的形式提供给开发者，通过使用T2SDK开发程序可以轻松实现和CRES2.0（金融基础件2.0）平台的对接，使用T2_SDK开发的程序在本文档中也称为“客户端”
--- t2sdk/t2sdk_test.go
+++ t2sdk/t2sdk_test.go
@@ -23,74 +23,72 @@ func (req Request_105010) ParseRequestContent() uintptr {
 	return packer
 }
 
-// func TestSyncSendMsg(t *testing.T) {
-// 	config := GetNewConfig()
-// 	iniFile := "t2sdk.ini"
-// 	ret := LoadT2Config(config, iniFile)
-// 	if ret != 0 {
-// 		t.Errorf("T2 Config Read Error,ErrorNo:%d", ret)
-// 	}
-// 	conn := GetNewConnection(config)
-// 	ret = InitCreat(conn)
-// 	if ret != 0 {
-// 		t.Errorf("InitCreat Error,ErrorNo:%d", ret)
-// 	}
-// 	ret_conn := T2Connect(conn, 1000)
-// 	if ret_conn != 0 {
-// 		err_info := GetErrorMsg(conn, ret_conn)
-// 		t.Errorf("T2Connect Error,ErrorNo:%d,ErrorInfo:%s", ret_conn, err_info)
-// 	} else {
-// 		t.Log("T2 Connection Ok...")
+//同步发包
+func TestSyncSendMsg(t *testing.T) {
+	config := GetNewConfig()
+	iniFile := "t2sdk.ini"
+	ret := LoadT2Config(config, iniFile)
+	if ret != 0 {
+		t.Errorf("T2 Config Read Error,ErrorNo:%d", ret)
+	}
+	conn := GetNewConnection(config)
+	ret = InitCreat(conn)
+	if ret != 0 {
+		t.Errorf("InitCreat Error,ErrorNo:%d", ret)
+	}
+	ret_conn := T2Connect(conn, 1000)
+	if ret_conn != 0 {
+		err_info := GetErrorMsg(conn, ret_conn)
+		t.Errorf("T2Connect Error,ErrorNo:%d,ErrorInfo:%s", ret_conn, err_info)
+	} else {
+		t.Log("T2 Connection Ok...")
 
-// 		var iq IRequest = Request_105010{105010}
-// 		ret = SyncSendMsg(conn, 105010, iq)
-// 		if ret < 0 {
-// 			t.Errorf("syncSendMsg Error,FunctionNo:%d,Error:%s", 105010, GetErrorMsg(conn, ret))
-// 		} else {
-// 			_, p := SyncRecvBiz(conn, ret)
-// 			c1 := GetDatasetCount(p)
-// 			for x := int32(0); x < c1; x++ {
-// 				c2 := GetColCount(p)
-// 				for y := int32(0); y < c2; y++ {
-// 					c_name := GetColName(p, y)
-// 					fmt.Printf(" %s", c_name)
-// 				}
-// 				fmt.Println("")
-// 				row_count := GetRowCount(p)
-// 				for y := int32(0); y < row_count; y++ {
-// 					for z := int32(0); z < c2; z++ {
-// 						c_type := GetColType(p, z)
-// 						switch c_type {
-// 						case "S":
-// 							fmt.Printf(" %s", GetStrByIndex(p, z))
-// 						case "C":
-// 							fmt.Printf(" %s", GetCharByIndex(p, z))
-// 						case "I":
-// 							fmt.Printf(" %d", GetIntByIndex(p, z))
-// 						case "F":
-// 							fmt.Printf(" %f", GetDoubleByIndex(p, z))
-// 						default:
-// 							fmt.Printf(" %s", c_type)
-// 						}
-// 					}
-// 					Next(p)
-// 					fmt.Println("")
-// 				}
-// 				t.Logf("dataTable,row_count:%d", row_count)
-// 			}
-// 			FreeMem(p)
-// 			Release(p)
-// 		}
-// 	}
-// 	Release(conn)
-// 	Release(config)
+		var iq IRequest = Request_105010{105010}
+		ret = SyncSendMsg(conn, 105010, iq)
+		if ret < 0 {
+			t.Errorf("syncSendMsg Error,FunctionNo:%d,Error:%s", 105010, GetErrorMsg(conn, ret))
+		} else {
+			_, p := SyncRecvBiz(conn, ret)
+			c1 := GetDatasetCount(p)
+			for x := int32(0); x < c1; x++ {
+				c2 := GetColCount(p)
+				for y := int32(0); y < c2; y++ {
+					c_name := GetColName(p, y)
+					fmt.Printf(" %s", c_name)
+				}
+				fmt.Println("")
+				row_count := GetRowCount(p)
+				for y := int32(0); y < row_count; y++ {
+					for z := int32(0); z < c2; z++ {
+						c_type := GetColType(p, z)
+						switch c_type {
+						case "S":
+							fmt.Printf(" %s", GetStrByIndex(p, z))
+						case "C":
+							fmt.Printf(" %s", GetCharByIndex(p, z))
+						case "I":
+							fmt.Printf(" %d", GetIntByIndex(p, z))
+						case "F":
+							fmt.Printf(" %f", GetDoubleByIndex(p, z))
+						default:
+							fmt.Printf(" %s", c_type)
+						}
+					}
+					Next(p)
+					fmt.Println("")
+				}
+				t.Logf("dataTable,row_count:%d", row_count)
+			}
+			FreeMem(p)
+			Release(p)
+		}
+	}
+	Release(conn)
+	Release(config)
 
-// }
+}
 
-// func TestXXX(t *testing.T){
-// 	AskForCallback()
-// }
-
+//异步发包
 func TestAsynSendMsg(t *testing.T) {
 	config := GetNewConfig()
 	iniFile := "t2sdk.ini"
@@ -134,6 +132,8 @@ func TestAsynSendMsg(t *testing.T) {
 			}
 			t.Logf("dataTable,row_count:%d", row_count)
 		}
+		FreeMem(lpUnPackerOrStr)
+		Release(lpUnPackerOrStr)
 		ch <- nResult
 		return 0
 	}
